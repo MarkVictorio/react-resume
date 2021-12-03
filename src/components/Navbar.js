797418689058158079a5b617afe5fc9bd02ed950
Mark Victorio <mvictorio79@gmail.com>
2021-12-04 03:46:36 +0800

    Final-for Now RiP Bulma Site

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,24 +28,6 @@ function Navbar() {
                   About
                 </a>
 
-                <div className="navbar-item has-dropdown is-hoverable">
-                  <a className="navbar-link">
-                    More
-                  </a>
-
-                  <div className="navbar-dropdown">
-                    <a className="navbar-item" href = "https://github.com/MarkVictorio?tab=repositories">
-                      My Github Repository
-                    </a>
-                    <a className="navbar-item">
-                      Contact
-                    </a>
-                    <hr className="navbar-divider" />
-                    <a className="navbar-item">
-                      Report an issue
-                    </a>
-                  </div>
-                </div>
               </div>
 
             </div>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faYoutubeSquare,faTwitterSquare, faFacebookSquare,
-     faJsSquare, faPython, faReact, faBootstrap, faHtml5,faCss3Alt,faSass} from "@fortawesome/free-brands-svg-icons"
-import { faCode, faFlask } from "@fortawesome/free-solid-svg-icons"
+     faJsSquare, faPython, faReact, faBootstrap, faHtml5,faCss3Alt,faSass, faJava} from "@fortawesome/free-brands-svg-icons"
+import { faCode } from "@fortawesome/free-solid-svg-icons"
 function Resume() {
     return (
         <div className="columns m-6">
+            <div className="column">
+                <section className ="mt-6">
+                    <p className="has-text-white">I am familiar with several programming languages, I am mostly comfortable using python, but I am gaining more and more interest in JavaScript and Typescript. Although I am not limited to these two the rest of the Programming Languages and Frameworks that I am familiar with are listed below. </p>
+                    <h1 className="title has-text-white is-size-5 mt-4"><FontAwesomeIcon icon={faCode} className="title is-size-5 mx-1 has-text-white-ter" />Programming Languages:</h1>
+                </section>
+                <section className="ml-6 my-3 is-flex">
+                    <FontAwesomeIcon icon={faJsSquare} className="title is-size-1 mx-1 has-text-white-ter"/>
+                    <FontAwesomeIcon icon={faPython} className="title is-size-1 mx-1 has-text-white-ter"/>
+                    <FontAwesomeIcon icon={faReact} className="title is-size-1 mx-1 has-text-white-ter"/>
+                    <FontAwesomeIcon icon={faBootstrap} className="title is-size-1 mx-1 has-text-white-ter"/>
+                </section>
+                <section className="ml-6 is-flex">
+                    <FontAwesomeIcon icon={faJava} className="title is-size-1 mx-2 has-text-white-ter"/>
+                    <FontAwesomeIcon icon={faHtml5} className="title is-size-1 mx-2 has-text-white-ter"/>
+                    <FontAwesomeIcon icon={faCss3Alt} className="title is-size-1 mx-2 has-text-white-ter"/>
+                    <FontAwesomeIcon icon={faSass} className="title is-size-1 mx-1 has-text-white-ter"/>
+                </section>
+                <h1 className="title is-size-5 has-text-white ml-6 my-3"> MySQL</h1>
+                <h1 className="title is-size-5 has-text-white ml-6 my-3"> Google Firebase</h1>
+                <h1 className="title is-size-5 has-text-white ml-6 my-3"> MongoDB </h1>
+            </div>
             <div className="column has-text-centered">
                 <figure className="image is-128x128 is-inline-block">
                     <img src="/images/profileimage.jpg" className = "is-rounded"/>
@@ -25,28 +46,6 @@ function Resume() {
                 <a href="https://www.youtube.com/c/OnTheMark"><FontAwesomeIcon icon={faYoutubeSquare} className="title is-size-1 mx-1 has-text-white-ter" /></a>
                 </section>
                 
-            </div>
-            <div className="column">
-                <section className ="mt-6">
-                    <p className="has-text-white">I am familiar with several programming languages, I am mostly comfortable using python, but I am gaining more and more interest in JavaScript and Typescript. Although I am not limited to these two the rest of the Programming Languages and Frameworks that I am familiar with are listed below. </p>
-                    <h1 className="title has-text-white is-size-5 mt-4"><FontAwesomeIcon icon={faCode} className="title is-size-5 mx-1 has-text-white-ter" />Programming Languages:</h1>
-                </section>
-                <section className="ml-6 my-3 is-flex">
-                    <FontAwesomeIcon icon={faJsSquare} className="title is-size-1 mx-1 has-text-white-ter"/>
-                    <FontAwesomeIcon icon={faPython} className="title is-size-1 mx-1 has-text-white-ter"/>
-                    <FontAwesomeIcon icon={faReact} className="title is-size-1 mx-1 has-text-white-ter"/>
-                    <FontAwesomeIcon icon={faBootstrap} className="title is-size-1 mx-1 has-text-white-ter"/>
-                </section>
-                <section className="ml-6 is-flex">
-                    <FontAwesomeIcon icon={faFlask} className="title is-size-1 mx-1 has-text-white-ter"/>
-                    <FontAwesomeIcon icon={faHtml5} className="title is-size-1 mx-1 has-text-white-ter"/>
-                    <FontAwesomeIcon icon={faCss3Alt} className="title is-size-1 mx-1 has-text-white-ter"/>
-                    <FontAwesomeIcon icon={faSass} className="title is-size-1 mx-1 has-text-white-ter"/>
-                </section>
-                <h1 className="title is-size-5 has-text-white ml-6 my-3"> MySQL</h1>
-                <h1 className="title is-size-5 has-text-white ml-6 my-3"> Google Firebase</h1>
-                <h1 className="title is-size-5 has-text-white ml-6 my-3"> MongoDB </h1>
-
             </div>
             <div className="column has-text-white">
                 <section className ="mt-6">
